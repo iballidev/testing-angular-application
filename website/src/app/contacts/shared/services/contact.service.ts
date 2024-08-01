8e095ae5d8f59c9f1f5aee9be621fc25aac7662c
iballi2017 <iballi201@gmail.com>
2024-08-02 00:15:13 +0100

    update

--- website/src/app/contacts/shared/services/contact.service.ts
+++ website/src/app/contacts/shared/services/contact.service.ts
@@ -1,27 +1,34 @@
-import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Injectable } from "@angular/core";
+import { Headers, Http } from "@angular/http";
 
-import 'rxjs/add/operator/toPromise';
+// import 'rxjs/add/operator/toPromise';
 
-import { Contact } from '../';
+import { Contact } from "../";
+import { catchError, map } from "rxjs/operators";
 
 @Injectable()
 export class ContactService {
-  private contactsUrl = 'app/contacts';
-  private headers: Headers = new Headers({'Content-Type': 'application/json'});
+  private contactsUrl = "app/contacts";
+  private headers: Headers = new Headers({
+    "Content-Type": "application/json",
+  });
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
   public getContacts(): any {
-    return this.http.get(this.contactsUrl)
-               .toPromise()
-               .then(response => response.json().data as Contact)
-               .catch(this.handleError);
+    return (
+      this.http
+        .get(this.contactsUrl)
+         .toPromise()
+         .then(response => response.json().data as Contact)
+         .catch(this.handleError)
+    );
   }
 
   public getContact(id: number): Promise<Contact> {
-    return this.getContacts()
-               .then(contacts => contacts.find(contact => contact.id === id));
+    return this.getContacts().then((contacts) =>
+      contacts.find((contact) => contact.id === id)
+    );
   }
 
   public save(contact: Contact): Promise<Contact> {
@@ -40,32 +47,34 @@ export class ContactService {
     const url = `${this.contactsUrl}/${contact.id}`;
 
     return this.http
-             .delete(url, {headers: this.headers})
-             .toPromise()
-             .then(() => null)
-             .catch(this.handleError);
+      .delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
   }
 
   public post(contact: Contact): Promise<Contact> {
     return this.http
-        .post(this.contactsUrl, JSON.stringify(contact), {headers: this.headers})
-        .toPromise()
-        .then(res => res.json())
-        .catch(this.handleError);
+      .post(this.contactsUrl, JSON.stringify(contact), {
+        headers: this.headers,
+      })
+      .toPromise()
+      .then((res) => res.json())
+      .catch(this.handleError);
   }
 
   public put(contact: Contact): Promise<Contact> {
     const url = `${this.contactsUrl}/${contact.id}`;
 
     return this.http
-             .put(url, JSON.stringify(contact), {headers: this.headers})
-             .toPromise()
-             .then(() => contact)
-             .catch(this.handleError);
+      .put(url, JSON.stringify(contact), { headers: this.headers })
+      .toPromise()
+      .then(() => contact)
+      .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error);
+    console.error("An error occurred", error);
     return Promise.reject(error.message || error);
   }
 }
